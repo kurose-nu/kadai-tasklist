f23270c92f0a9f72b6e82211da56662033d5a4c1
kurose-nu <yuta.saijyo12@gmail.com>
2019-06-25 21:38:53 +0000

    user signup

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # 小文字に変換
+  before_save {self.email.downcase!}
+  # nameのバリデーション
+  validates :name, presence: true, length: {maximum:50}
+  # emailのバリデーション
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  # パスワードを暗号化して保存する
+  has_secure_password
+end
